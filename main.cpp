548829788b1022831d01d66e580ca350f41422ea
ivanprosh <ivanprosh@gmail.com>
2016-07-12 23:29:41 +0300

    Не доработано чтение из файла

---
+++ main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+const int size = 6;
+int PhoneNumber[size];
+
+int Num_count = 0;
+//массивы предикатов
+typedef bool (*predicat)(int *it1, int *it2);
+typedef pair<predicat,int*> condition;
+typedef vector<condition>::const_iterator My_iterator;
+
+bool equal(int *it1, int *it2)
+{
+    return *it1==*it2;
+}
+bool numless(int *it1, int *it2)
+{
+    return (*it1)<(*it2);
+}
+bool numhigh(int *it1, int *it2)
+{
+    return (*it1)>(*it2);
+}
+bool numhighequal(int *it1, int *it2)
+{
+    return !numless(it1,it2);
+}
+bool numlessequal(int *it1, int *it2)
+{
+    return !numhigh(it1,it2);
+}
+bool notequal(int *it1, int *it2)
+{
+    return !equal(it1,it2);
+}
+
+bool rfile(const string& name, vector<condition>* Vec)
+{
+    ifstream file;
+    file.open(name);
+
+    int a,b;
+    char c[2];
+
+    if(file.is_open()) {
+        while(!file.eof())
+        {
+            file >> a;
+            >> c >> b;//
+            cout << a << " " << c[0] << " " << c[1] << " " << b << endl;
+            switch (c[0]) {
+            case '<':
+                (a < b) ? Vec[b-1].push_back(condition(numhigh,&PhoneNumber[a-1])):
+                          Vec[a-1].push_back(condition(numless,&PhoneNumber[b-1]));
+                break;
+            case '=':
+                (a < b) ? Vec[b-1].push_back(condition(equal,&PhoneNumber[a-1])):
+                          Vec[a-1].push_back(condition(equal,&PhoneNumber[b-1]));
+                break;
+            case '>':
+                (a < b) ? Vec[b-1].push_back(condition(numless,&PhoneNumber[a-1])):
+                          Vec[a-1].push_back(condition(numhigh,&PhoneNumber[b-1]));
+                break;
+            case '<>':
+                (a < b) ? Vec[b-1].push_back(condition(notequal,&PhoneNumber[a-1])):
+                          Vec[a-1].push_back(condition(notequal,&PhoneNumber[b-1]));
+                break;
+            case '<>':
+                (a < b) ? Vec[b-1].push_back(condition(notequal,&PhoneNumber[a-1])):
+                          Vec[a-1].push_back(condition(notequal,&PhoneNumber[b-1]));
+                break;
+            default:
+                break;
+            }
+        }
+        file.close ();
+        return 1;
+    }
+    else
+    {
+        cerr << "File not found" << endl;
+        return 0;
+    }
+}
+void init(vector<condition>* Vec)
+{
+    Vec[1].push_back(condition(numhigh,&PhoneNumber[0]));
+    Vec[2].push_back(condition(equal,&PhoneNumber[0]));
+    Vec[4].push_back(condition(numless,&PhoneNumber[2]));
+    //Vec[4].push_back(curEl);
+}
+
+bool analyze(const vector<condition>& vec,int index)
+{
+    if(!vec.empty()){
+        My_iterator it = vec.begin();
+        while(it!=vec.end())
+        {
+            if(it->first(&PhoneNumber[index],it->second)) it++;
+            else return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    vector<condition> *Func = new vector<condition>[size];
+    if(!rfile("g:\\WORK\\_QT\\_FREELANCE\\Combinatorika\\PhoneNumbers\\input.txt",Func)) return 0;
+    //init(Func);
+    //время выполнения
+    int start = clock();
+
+    for(PhoneNumber[0]=0;PhoneNumber[0]<10;PhoneNumber[0]++)
+    {
+        if(!analyze(Func[0],0)) continue;
+        for(PhoneNumber[1]=0;PhoneNumber[1]<10;PhoneNumber[1]++)
+        {
+            if(!analyze(Func[1],1)) continue;
+            for(PhoneNumber[2]=0;PhoneNumber[2]<10;PhoneNumber[2]++)
+            {
+                if(!analyze(Func[2],2)) continue;
+                for(PhoneNumber[3]=0;PhoneNumber[3]<10;PhoneNumber[3]++)
+                {
+                    if(!analyze(Func[3],3)) continue;
+                    for(PhoneNumber[4]=0;PhoneNumber[4]<10;PhoneNumber[4]++)
+                    {
+                        if(!analyze(Func[4],4)) continue;
+                        for(PhoneNumber[5]=0;PhoneNumber[5]<10;PhoneNumber[5]++)
+                        {
+                            if(!analyze(Func[5],5)) continue;
+                            Num_count++;
+                        }
+                    }
+                }
+            }
+            int count = PhoneNumber[1];
+            count = PhoneNumber[1];
+        }
+    }
+    //останов
+    int stop = clock();
+    cout << ((float)(stop-start)) / CLOCKS_PER_SEC << " seconds" << endl;
+    cout << "Count is " << Num_count;
+    return 0;
+}
